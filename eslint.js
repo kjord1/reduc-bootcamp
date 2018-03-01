f830df88f48b10a8934111000ce3ebd6b2f4f747
Richard Kent Jordan <kent.jordan@monsanto.com>
2018-03-01 10:14:54 -0600

    Implement eslint

---
+++ eslint.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: '@monsantoit/eslint-config-velmat-inventory',
+  plugins: ['react'],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jasmine: true,
+    jest: true
+  }
+};
