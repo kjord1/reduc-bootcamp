7ad2176eb45eb1ba05df9435d137afcab8a8ec40
Richard Kent Jordan <kent.jordan@monsanto.com>
2018-03-01 12:03:28 -0600

    Componentized

---
+++ src/components/profile_name.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ProfileNameComponent = ({profileName}) => (
+  <div>
+    <h1>
+      <span className="fa fa-user" aria-hidden="true"/>&nbsp;
+      {profileName}
+    </h1>
+  </div>
+);
+
+ProfileNameComponent.displayName = 'ProfileNameComponent';
+ProfileNameComponent.propTypes = {
+  profileName: PropTypes.string.isRequired
+};
+
+export default ProfileNameComponent;
--- src/components/root.jsx
+++ src/components/root.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
-import reducers from '../reducers'
 
-const store = createStore(reducers);
+import reducers from '../reducers';
+import ProfileNameComponent from './profile_name'
+
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const RootComponent = () => (<Provider store={store}>
   <div className="containter">
@@ -14,12 +19,7 @@ const RootComponent = () => (<Provider store={store}>
       </h1>
     </div>
     <br/>
-    <div>
-      <h1>
-        <span className="fa fa-user" aria-hidden="true"/>&nbsp;
-        Kent Jordan
-      </h1>
-    </div>
+    <ProfileNameComponent profileName="Kent Jordan" />
   </div>
 </Provider>);
 RootComponent.displayName = 'RootComponent';
